52adb7a8b57fbd1fe49fd01430175e3e32ccdad0
MatteoVollero <m.vollero120796@gmail.com>
2020-10-26 18:17:22 +0100

    Un po di stile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,27 @@
-<table>
-  <thead>
-    <th>Nome</th>
-    <th>Cognome</th>
-    <th>Date Of Birth</th>
-  </thead>
-  @foreach ($students as $student)
-    <tr>
-      <td>{{$student->name}}</td>
-      <td>{{$student->lastname}}</td>
-      <td>{{$student->date_of_birth}}</td>
-    </tr>
-  @endforeach
-</table>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+  <body>
+
+    <table>
+      <thead>
+        <th>Nome</th>
+        <th>Cognome</th>
+        <th>Date Of Birth</th>
+      </thead>
+      @foreach ($students as $student)
+        <tr>
+          <td>{{$student->name}}</td>
+          <td>{{$student->lastname}}</td>
+          <td>{{$student->date_of_birth}}</td>
+        </tr>
+      @endforeach
+    </table>
+
+
+  </body>
+</html>
